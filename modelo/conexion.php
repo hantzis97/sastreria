d50430079e6e0724aaac119d24430788d02dc659
FelixEIGato <tcin@outlook.es>
2021-01-28 22:53:36 -0500

    commit

---
+++ modelo/conexion.php
@@ -0,0 +1,26 @@
+<?php
+
+class Conexion{
+
+	private $conexion;
+
+	function __construct(){
+		#probando 123
+		$this->conexion = mysqli_connect("localhost","user","password","bd");
+		$this->conexion->set_charset("utf8");
+	}
+
+	public function obtenerConexion(){
+		return $this->conexion;
+	}	
+
+	public function query($sql){
+		return mysqli_query($this->conexion,$sql);
+	}
+
+	public function cerrarConexion(){
+		mysqli_close($this->conexion);
+	}
+}
+
+?>
